80186e131fc252678689ec3db02523578cc71fab
gjwldbs <bearjamie@naver.com>
2026-04-11 00:27:08 +0900

    good
    영상 업로드해서 pose estimation

--- main.py
+++ main.py
@@ -1,31 +1,51 @@
-import cv2
+import cv2 #to read and process images
+import matplotlib.pyplot as plt #to show resultant images 
 import mediapipe as mp
-import time
 
-mp_hands = mp.solutions.hands
-mp_draw = mp.solutions.drawing_utils
+# Initializing mediapipe pose class.
+mp_pose = mp.solutions.pose
+# Setting up the Pose model for images.
+pose_img = mp_pose.Pose(static_image_mode=True, min_detection_confidence=0.5, model_complexity=1)
+# Setting up the Pose model for videos.
+pose_video = mp_pose.Pose(static_image_mode=False, min_detection_confidence=0.5, 
+min_tracking_confidence=0.5, model_complexity=1)
 
-cap = cv2.VideoCapture(0)
-cap.set(3, 1280)
-cap.set(4, 720)
+# Initializing mediapipe drawing class to draw landmarks on specified image.
+mp_drawing = mp.solutions.drawing_utils
 
-def main():
-    while True:
-        attempt = 0
-        success, img = cap.read()
-        while not success and attempt <5:
-            time.sleep(0.2)
-            success, img = cap.read()
-            attempt +=1
-        if not success:
-            print(f"failid to read frame after {attempt} attemps.")
-            break
-        img = cv2.flip(img, 1)
-        cv2.imshow("Image", img)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    cap.release()
-    cv2.destroyAllWindows()
+def estimPose_video(input_file, pose_video=pose_video, landmarks_c=(234,63,247), connection_c=(117,249,77), thickness=5, circle_r=5, display=True, nrows_frames=4, ncols_frames=3):
     
-if __name__ == "__main__":
-    main()
+    # Initialize the VideoCapture object to read from a video stored in the disk.
+    video = cv2.VideoCapture(input_file)
+    
+    total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    frames = []
+    
+    for i in range(total_frames):
+        # Read a frame.
+        ok, frame = video.read()
+    
+        # Check if frame is not read properly.
+        if not ok:
+            # Break the loop.
+            break
+        
+        # Get the width and height of the frame
+        frame_height, frame_width, _ =  frame.shape
+        # Resize the frame while keeping the aspect ratio.
+        frame = cv2.resize(frame, (int(frame_width * (640 / frame_height)), 640))
+        frame, _ = estimPose_img(frame, pose_video, landmarks_c, connection_c, thickness, circle_r, display=False)
+    
+        frames.append(frame)
+    
+    if display :
+        fig, axarr = plt.subplots(nrows_frames,ncols_frames,figsize=[15, 15])
+        k=0
+        for i in range(nrows_frames):
+            for j in range(ncols_frames):
+                axarr[i,j].imshow(frames[k][:,:,::-1]);axarr[i,j].axis('off')
+                k+=1
+    else:
+        return frames
+    
+estimPose_video("C:\Users\dimas\OneDrive\사진\Camera Roll\WIN_20260409_11_28_32_Pro.mp4", nrows_frames=4, ncols_frames=5)
