b87179f3889959ec71807cd7fff2f25d30b4a41e
gjwldbs <bearjamie@naver.com>
2026-04-11 01:49:24 +0900

    시험 망함~~~by 허
    경로 입력하면 20프레임? pose estimate하는 거

--- main.py
+++ main.py
@@ -13,6 +13,53 @@ min_tracking_confidence=0.5, model_complexity=1)
 # Initializing mediapipe drawing class to draw landmarks on specified image.
 mp_drawing = mp.solutions.drawing_utils
 
+def estimPose_img(input_file, pose=pose_img, landmarks_c=(234,63,247), connection_c=(117,249,77), thickness=20, circle_r=10, display=True):
+    
+    # Read the input image
+    if isinstance(input_file, str) :
+        input_img = cv2.imread(input_file)
+    else :
+        input_img = input_file
+    
+    # Create a copy of the input image
+    output_img = input_img.copy()
+    
+    # Convert the image from BGR into RGB format.
+    RGB_img = cv2.cvtColor(output_img, cv2.COLOR_BGR2RGB)
+    
+    # Perform the Pose Detection.
+    results = pose.process(RGB_img)
+    
+    # Retrieve the height and width of the input image.
+    height, width, _ = input_img.shape
+    
+    # Initialize a list to store the detected landmarks.
+    landmarks = []
+    
+    # Check if any landmarks are detected.
+    if results.pose_landmarks:
+    
+        # Draw Pose landmarks on the output image.
+        mp_drawing.draw_landmarks(output_img, results.pose_landmarks, mp_pose.POSE_CONNECTIONS, mp_drawing.DrawingSpec(landmarks_c, thickness, circle_r), mp_drawing.DrawingSpec(connection_c, thickness, circle_r))
+        
+        # Iterate over the detected landmarks.
+        for landmark in results.pose_landmarks.landmark:
+            landmarks.append((int(landmark.x * width), int(landmark.y * height), (landmark.z * width)))
+    # Check if we want to display.
+    if display:
+        # Display the original input image and the resulting image.
+        plt.figure(figsize=[15,15])
+        plt.subplot(121);plt.imshow(input_img[:,:,::-1]);plt.title("Original image");plt.axis('off');
+        plt.subplot(122);plt.imshow(output_img[:,:,::-1]);plt.title("Output image");plt.axis('off');
+        
+        # Plot the Pose landmarks in 3D.
+        mp_drawing.plot_landmarks(results.pose_world_landmarks, mp_pose.POSE_CONNECTIONS)
+        
+    # Just get output_img and landmarks
+    else:
+        # Return the output image and the found landmarks.
+        return output_img, landmarks
+
 def estimPose_video(input_file, pose_video=pose_video, landmarks_c=(234,63,247), connection_c=(117,249,77), thickness=5, circle_r=5, display=True, nrows_frames=4, ncols_frames=3):
     
     # Initialize the VideoCapture object to read from a video stored in the disk.
@@ -48,4 +95,11 @@ def estimPose_video(input_file, pose_video=pose_video, landmarks_c=(234,63,247),
     else:
         return frames
     
-estimPose_video("C:\Users\dimas\OneDrive\사진\Camera Roll\WIN_20260409_11_28_32_Pro.mp4", nrows_frames=4, ncols_frames=5)
+video_path = input("분석할 영상의 경로: ")
+video_path = video_path.strip().replace('"', '')
+
+estimPose_video(video_path, nrows_frames=4, ncols_frames=5)
+
+plt.savefig("result_grid.png") 
+plt.show()
+print("격자 이미지가 저장되었습니다.")
